cb916837e5f9b1e9709b50a094710f8570c9e813
ManuRP99 <manuelrampat@gmail.com>
2024-02-15 14:09:38 +0100

    first python

---
+++ python/pythonStrings.py
@@ -0,0 +1,12 @@
+
+
+course = 'Python for beginners'
+
+print(course)
+print(course.upper())
+print(course.lower())
+print(course.find('n')) #returns the index of the letter n
+print(course.replace('for', '4'))
+print('Python' in course) # returns tru if the string contains Python
+
+
---
+++ python/sumCalculator.py
@@ -0,0 +1,17 @@
+#first_string = input('First number: ')
+#second_string = input('Second number: ')
+
+#first_number = float(first_string)
+#second_number = float(second_string)
+
+#answer = first_number + second_number
+#answer_string = str(answer)
+
+
+# float them directly to save lines
+first = float(input('First number: '))
+second = float(input('First number: '))
+
+print('thinking...')
+#print('The sum is ' + answer_string)
+print(first + second)
