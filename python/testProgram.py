a0ea8908dcb58455356102d074f12f75e6abd885
ManuRP99 <manuelrampat@gmail.com>
2024-02-15 14:56:14 +0100

    array tables pngs

---
+++ python/testProgram.py
@@ -0,0 +1,12 @@
+
+from array import *
+
+arr = array('i', [1, 2, 3, 4, 5])
+arr_2 = array('u', ['a', 's', 'd', 'f'])
+print(arr)
+print(arr_2)
+print(arr_2[0])
+print(arr_2[1])
+print(arr_2[2])
+print(arr_2[3])
+
