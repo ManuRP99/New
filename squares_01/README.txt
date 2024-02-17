86927110f6926e5c76cab26b8f75ccb2bcb08aec
ManuRP99 <manuelrampat@gmail.com>
2024-02-17 22:27:27 +0100

    squares_01_ .1

---
+++ squares_01/README.txt
@@ -0,0 +1,53 @@
+README
+
+
+down there is what i was going to do. hwat i did is a control layout for moving a rectangle and making you loose if you touch the edges
+
+
+
+would be cool to randomly spawn foor to catch and to add more divs for the controls(or use the keys)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+There will be a white square in the middle
+
+there will be red squares appearing at a random place on the canvas, and moving towards the white square
+
+the white square shoots small square that kill a red square if they touch them 
+
+
+
+
+we need to:
+
+//calculate square sizes based on window.height and window.width
+
+//create the white square in the center of the screen
+
+create the red squares in a random place of the edge and move them to the white square
+
+make the bullets appear 
+	make a way to control the bullet direction
+
+make the collision detection for the red and white squares
+
+
+TANGENTS
+
+4 invisible buttons that span a quarter of the screen each
+
+	for now move the white square to that direction
---
+++ squares_01/script.js
@@ -0,0 +1,99 @@
+// so far this moves very fast in diagonals, change the hatml div elements to change how it reacts
+
+// ~/Desktop/webdev/pieces/bigcanvas
+//
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+
+canvas.width = (window.innerWidth - 35);
+canvas.height = (window.innerHeight - 50);
+
+
+//
+// make the buttons div same size as the canvas
+const buttonsDiv = document.querySelector('#buttons-div');
+
+buttonsDiv.width = (window.innerWidth - 35);
+buttonsDiv.height = (window.innerHeight - 50);
+
+
+//
+// get width and height units for square sizes
+const widthUnit = Math.floor(canvas.width * 10) / 1000;
+const heightUnit = Math.floor(canvas.height * 10) / 1000;
+
+//
+// create white square
+
+let whSqX = 45;
+let whSqY = 45;
+let whSqVX = 1;
+let whSqVY = 1;
+
+const whiteSquare = () => {
+	ctx.clearRect(((whSqX - 1) * widthUnit),((whSqY - 1) * heightUnit),(12 * widthUnit),(12 * heightUnit));
+	whSqX += whSqVX;
+	whSqY += whSqVY;
+
+	ctx.fillStyle = '#ffffff';
+	// ctx.fillrect((),(),(),());
+	ctx.fillRect((whSqX * widthUnit),(whSqY * heightUnit),(10 * widthUnit),(10 * heightUnit));
+}
+
+// declare the control divs and give them functions
+const controlTL = document.querySelector('#ctrl-tl');
+const controlTR = document.querySelector('#ctrl-tr');
+const controlBL = document.querySelector('#ctrl-bl');
+const controlBR = document.querySelector('#ctrl-br');
+
+
+// change this to change what the controls do
+let tlHover = () => moveWhSq(-1, -1);
+
+let trHover = () => moveWhSq(1, -1);
+
+let blHover = () => moveWhSq(-1, 1);
+
+let brHover = () => moveWhSq(1, 1);
+
+
+
+
+const moveWhSq = (x, y) => {
+	whSqVX = x;
+	whSqVY = y;
+	whiteSquare();
+	whSqVX = 0;
+	whSqVY = 0;
+	if (whSqX < 0) {
+	ctx.clearRect(((whSqX - 1) * widthUnit),((whSqY - 1) * heightUnit),(12 * widthUnit),(12 * heightUnit));
+		whSqX = 45;
+		whSqY = 45;
+	}
+	else if (whSqY < 0) {
+		ctx.clearRect(((whSqX - 1) * widthUnit),((whSqY - 1) * heightUnit),(12 * widthUnit),(12 * heightUnit));
+		whSqX = 45;
+		whSqY = 45;
+	}
+	else if (whSqX > 90) {
+		ctx.clearRect(((whSqX - 1) * widthUnit),((whSqY - 1) * heightUnit),(12 * widthUnit),(12 * heightUnit));
+		whSqX = 45;
+		whSqY = 45;
+	} 
+	else if (whSqY > 90) {
+		ctx.clearRect(((whSqX - 1) * widthUnit),((whSqY - 1) * heightUnit),(12 * widthUnit),(12 * heightUnit));
+		whSqX = 45;
+		whSqY = 45;
+	}
+}
+
+
+
+whiteSquare();
+	// ctx.fillRect((45 * widthUnit),(45 * heightUnit),(10 * widthUnit),(10 * heightUnit));
+	//
+	//
+	
+	
+	
